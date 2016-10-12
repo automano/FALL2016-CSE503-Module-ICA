b3f0a6b6e77e7acd1890807663828c6c567f9179
Zhening Li <zhening.li@wustl.edu>
2016-10-12 13:02:40 -0500

    no message

--- profile-submit.php
+++ profile-submit.php
@@ -1,16 +1,65 @@
 <?php
-
-    if(isset($_GET['name'])){
-            
+//error description
+if(!isset($_POST['name'])){
+    echo "Please input your name!";
+}
+if(!isset($_POST['email'])){
+    echo "Please input your email!";
+}
+if(!isset($_POST['age'])){
+    echo "Please input your age!";
+}
+if(!isset($_POST['description'])){
+    echo "Please input your description!";
+}
+//http://www.w3schools.com/php/php_file_upload.asp
+$target_dir = "/uploads/";
+$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST['submit'])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
-    
-    //save picture
-    $full_path = sprintf("../uploads/%s",$filename);
-    if( move_uploaded_file($_FILES['uploadpicture']['tmp_name'], $full_path) ){
-        exit;
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
     }
-  
+}
+echo "<br>".$target_dir."<br>";
+echo "<br>".$target_file."<br>";
+echo "<br>".$imageFileType."<br>";
     $pictureUrl = sprintf("uploads/%s",$filename);
+    
     //insert new rows
 	$stmt = $mysqli->prepare("insert into users (name,email,age,description,pictureUrl) values (?,?,?,?,?)");
 	if(!$stmt){
@@ -21,10 +70,9 @@
 	$stmt->bind_param('ssiss', $name, $email,$age,$description,$pictureUrl);
 	$name = $_POST['name'];
     $email = $_POST['email'];
-    $age = $_POST['age'];
+    $age = int($_POST['age']);
     $description = $_POST['description'];
     $pictureUrl = sprintf("uploads/%s",$filename); 
-
 	$stmt->execute();
 	$stmt->close();
     header("Location: show-users.php");
